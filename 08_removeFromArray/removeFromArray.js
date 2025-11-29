8e144e852dc30fdbd6216af8522344a8e36e474c
Nil Monreal <nilmonreal@gmail.com>
2025-11-29 10:24:10 +0100

    08 Completed

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, toRemove) {
+    myArray.splice(myArray.indexOf(toRemove), 1);
+    return myArray;
 };
 
 // Do not edit below this line
