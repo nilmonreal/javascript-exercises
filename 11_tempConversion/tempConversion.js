edc55490efcefbdde05f41f5b87b330f3d4a1548
Nil Monreal <nilmonreal@gmail.com>
2025-11-29 10:47:17 +0100

    11 Completed Exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    return Math.round(((fahrenheit - 32) * 5 / 9)*100)/100;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    return Math.round(((celsius * 9 / 5) + 32)*100)/100;
 };
 
 // Do not edit below this line
